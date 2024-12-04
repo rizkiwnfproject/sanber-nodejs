e52138d28e6c498f29309b4d85fcb2dea0fe7a3d
rizkiwnf <rizkiwahyunf2@gmail.com>
2024-12-04 15:41:09 +0700

    tugas 1 + tugas 2

---
+++ tugas-2/loop.js
@@ -0,0 +1,13 @@
+// Buat program yang menggunakan loop forEach untuk mencetak angka 1 sampai 10.
+
+console.log("===================FOR===================" )
+for (let i = 0; i < 10; i++) {
+    console.log("number", i + 1);
+}
+
+numbers = [1,2,3,4,5,6,7,9,10]
+
+console.log("=================FOREACH=================" )
+numbers.forEach(number => {
+    console.log("number", number)
+});
